f5b01b6168a0b239afb06c2210858f575d22397c
Abdelrahman Salah <a.salah@msd-my.com>
2023-09-13 16:56:06 +0300

    Initial Commit

---
+++ PlentyPost/PlentyPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlentyPostApp.swift
+//  PlentyPost
+//
+//  Created by Abdelrahman Salah on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlentyPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
